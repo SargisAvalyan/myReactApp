e22da7db71c48b348b9e8634b0a50c0fd73b8a82
SargisAvalyan <sargis_90@mail.ru>
2022-11-10 00:07:37 +0400

    add menu

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import Menu from '../Menu';
 import "./style.scss";
 
 class Header extends React.Component {
   render() {
     return (
 
-      <div >
+      <div className='header-main'>
         <p className='header'>
-          UNITED COLORD OF BENETTON
+          UNITED COLORS OF BENETTON
         </p>
-
+        <Menu />
       </div>
 
     )
---
+++ src/Components/Menu/index.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import "./style.scss"
+
+class Menu extends React.Component {
+    render() {
+        return (
+            <div className='menu'>
+                <ul className='menu-header'>
+                    <li>МОДА</li>
+                    <li>ВЕЩИ</li>
+                    <li>ТРЕНДЫ</li>
+                    <li>КОСМЕТИКА</li>
+                    <li>ЗДОРОВЬЕ</li>
+                    <li>BEAUTY KIT</li>
+                </ul>
+
+            </div>
+
+
+
+        )
+    }
+}
+export default Menu
